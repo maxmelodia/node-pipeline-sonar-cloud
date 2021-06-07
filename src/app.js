aa23d77fcc21973a27ee482bec9851dcf61631df
Maxwell Duarte <maxmelodia@gmail.com>
2021-06-07 14:02:28 -0300

    teste

--- src/app.js
+++ src/app.js
@@ -8,6 +8,30 @@ function print(a,b) {
     console.log(`${a} - ${b} = ${subtracao(a,b)}`)
     console.log(`${a} * ${b} = ${multiplicacao(a,b)}`)
     console.log(`${a} / ${b} = ${divisao(a,b)}`)
+
+    //testando bad smells
+    if (1==1){
+        if (2==2){
+            if (3==3){
+                if (4==4){
+                    if (5==5){
+                        if (6==6){
+                            if (7==7){
+                                if (8==8){
+                                    if (9==9){
+                                        if(10==10){
+                                            console.log('Testando badsmells -> código hadouken!')
+                                        }
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+
 }
 
 print(10,2);
